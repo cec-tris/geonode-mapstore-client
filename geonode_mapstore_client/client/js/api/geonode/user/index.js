e6f2d33e0fbf9a8b68ac064ab4c1e5d00b7bab0e
chumano <xuanloc_hoang2000@yahoo.com>
2023-06-26 17:38:54 +0700

    parseDevHostname o/v4/userinfo

--- geonode_mapstore_client/client/js/api/geonode/user/index.js
+++ geonode_mapstore_client/client/js/api/geonode/user/index.js
@@ -7,7 +7,7 @@
  */
 
 import axios from '@mapstore/framework/libs/ajax';
-import { getGeoNodeLocalConfig } from "@js/utils/APIUtils";
+import { getGeoNodeLocalConfig, parseDevHostname } from "@js/utils/APIUtils";
 
 /**
 * Api for GeoNode user
@@ -16,7 +16,8 @@ import { getGeoNodeLocalConfig } from "@js/utils/APIUtils";
 
 export const getUserInfo = (apikey) => {
     const endpointV1 = getGeoNodeLocalConfig('geoNodeApi.endpointV1', '/api');
-    return axios.get(`${endpointV1}/o/v4/userinfo`, {
+    const url = parseDevHostname(`${endpointV1}/o/v4/userinfo`)
+    return axios.get(url, {
         params: {
             ...(apikey && { apikey })
         }
